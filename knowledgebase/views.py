40bdf4e76ee2faab2e30934ce3d6ee8697b3a982
Daelan Scroggins <dscroggins@wonderwarenorth.com>
2016-02-16 06:35:07 -0800

    Next commit

---
+++ knowledgebase/views.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+
+def home(request):
+	return render(request, 'knowledgebase/index.html', {})
