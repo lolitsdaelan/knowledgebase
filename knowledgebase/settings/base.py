0f52734da1d8931e16b7e4964f34b47cc422bdbe
Daelan Scroggins <dscroggins@wonderwarenorth.com>
2016-02-16 06:57:12 -0800

    Not understanding static files

--- knowledgebase/settings/base.py
+++ knowledgebase/settings/base.py
@@ -15,6 +15,7 @@ import os
 from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -129,7 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = 'knowledgebase/staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
